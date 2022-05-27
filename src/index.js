581dba0840faf87c38097cec2c6a5a8593cef158
PianoArts2007 <92180085+PianoArts2007@users.noreply.github.com>
2022-05-27 12:57:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,15 +11,16 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
     titleBarStyle: "hiddenInset",
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.setMenuBarVisibility(false);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
